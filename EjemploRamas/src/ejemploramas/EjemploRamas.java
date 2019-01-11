239e3c8cd62233f5f8692a5a2b38d47262e2ccf1
dam <dam@172.26.219.133>
2019-01-11 14:08:15 +0100

    Version 1.1

--- EjemploRamas/src/ejemploramas/EjemploRamas.java
+++ EjemploRamas/src/ejemploramas/EjemploRamas.java
@@ -16,7 +16,7 @@ public class EjemploRamas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Version 1.0");
+        System.out.println("Version 1.1");
     }
 
 }
